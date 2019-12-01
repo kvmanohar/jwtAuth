d4b200b281615da24c422c2be03aec8e9e2bfed8
Manohar Kurapati <kvmanohar@gmail.com>
2019-12-01 13:02:30 +0000

    register end point completed

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const { verify } = require('jsonwebtoken');
 const { hash, compare } = require('bcryptjs');
 
-const { fakeDB } = require('..src/fakeDB.js');
+const { fakeDB } = require('../src/fakeDB');
 //1. Register a user
 //2. Login a user
 //3. Logout a user
@@ -33,7 +33,24 @@ server.post('/register', async (req, res) => {
 
 	try {
 		//1.Check if user exists
-	} catch (err) {}
+		const user = fakeDB.find((user) => user.email === email);
+		if (user) throw new Error('User already exists');
+
+		//2. If not user exits, hash the password
+		const hashedPassword = await hash(password, 10);
+
+		//3.Insert the user to the Database
+		fakeDB.push({
+			id: fakeDB.length,
+			email,
+			password: hashedPassword
+		});
+
+		res.send({ message: 'User created' });
+		console.log(fakeDB);
+	} catch (err) {
+		res.send({ error: `${err.message}` });
+	}
 });
 
 server.listen(process.env.PORT, () => {
