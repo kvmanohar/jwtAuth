795e2f923c020e3fa895a19ca8402b15242b33ad
Manohar Kurapati <kvmanohar@gmail.com>
2019-12-01 14:20:18 +0000

    login end point complete

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,7 @@ const { verify } = require('jsonwebtoken');
 const { hash, compare } = require('bcryptjs');
 
 const { fakeDB } = require('../src/fakeDB');
+const { createAccessToken, createRefreshToken, sendAccessToken, sendRefreshToken } = require(`./tokens`);
 //1. Register a user
 //2. Login a user
 //3. Logout a user
@@ -48,6 +49,35 @@ server.post('/register', async (req, res) => {
 
 		res.send({ message: 'User created' });
 		console.log(fakeDB);
+	} catch (err) {
+		res.send({ error: `${err.message}` });
+	}
+});
+
+//2.Login a user
+server.post('/login', async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		//1.Find user in the DB, if not exists send error
+		const user = fakeDB.find((user) => user.email === email);
+		if (!user) throw new Error('User does not exists');
+
+		//2. Compare crypted password and see if it matches
+		const valid = await compare(password, user.password);
+		if (!valid) throw new Error('Password not correct');
+
+		//3.Create refresh and access tokens
+		const accesstoken = createAccessToken(user.id);
+		const refreshToken = createRefreshToken(user.id);
+
+		//4.Put the referesh token in the DB
+		user.refreshToken = refreshToken;
+		console.log(fakeDB);
+
+		//5.Send token- Refresh token as cookie and access token as a regular response
+		sendRefreshToken(res, refreshToken);
+		sendAccessToken(res, req, accesstoken);
 	} catch (err) {
 		res.send({ error: `${err.message}` });
 	}
