205c4f7ae51baf521455ec512999051c90a438fe
Manohar Kurapati <kvmanohar@gmail.com>
2019-12-07 17:00:29 +0000

    Navigation and routing completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Router, navigate } from '@reach/router';
+
+import Navigation from './components/Navigation';
+import Login from './components/Login';
+import Register from './components/Register';
+import Protected from './components/Protected';
+import Content from './components/Content';
+
+export const userContext = React.createContext([]);
 
 function App() {
-	return <div className="App">App</div>;
+	const [user, setUser] = useState({});
+	const [loading, setLoading] = useState(true);
+
+	const logOutCallBack = async () => {};
+
+	useEffect(() => {}, []);
+
+	return (
+		<userContext.Provider value={[user, setUser]}>
+			<div className="App">
+				<Navigation logOutCallBack={logOutCallBack} />
+				<Router id="router">
+					<Login path="login" />
+					<Register path="register" />
+					<Protected path="protected" />
+					<Content path="/" />
+				</Router>
+			</div>
+		</userContext.Provider>
+	);
 }
 
 export default App;
